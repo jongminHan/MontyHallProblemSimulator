2044693bf9718acb9fc4d00c42afe66191cf6a05
Jongmin Han <33759102+jongminHan@users.noreply.github.com>
2019-07-29 11:04:04 +0900

    Designing UI

--- MontyHallProblemSimulator/Game.cpp
+++ MontyHallProblemSimulator/Game.cpp
@@ -1,5 +1,10 @@
 #include "Game.h"
 
+void signalHandler()
+{
+	std::cout << "clicked\n";
+}
+
 Game::Game()
 {
 }
@@ -29,8 +34,54 @@ void Game::Run()
 	
 	tgui::Texture closedDoorTexture("C:\\Users\\Jongmin\\Documents\\MontyHallProblemSimulator\\MontyHallProblemSimulator\\closedDoor.png");
 	tgui::Picture::Ptr door1 = tgui::Picture::create(closedDoorTexture);
-	door1->setPosition(150, 210);
+	tgui::Picture::Ptr door2 = tgui::Picture::create(closedDoorTexture);
+	tgui::Picture::Ptr door3 = tgui::Picture::create(closedDoorTexture);
+
+	door1->setPosition(150, 160);
+	door2->setPosition(500,160);
+	door3->setPosition(850, 160);
+
 	gui.add(door1);
+	gui.add(door2);
+	gui.add(door3);
+
+	door1->connect("Clicked", signalHandler);
+
+	tgui::Label::Ptr label1 = tgui::Label::create();
+	label1->setText("Run simulation");
+	label1->setSize(167.2, 48);
+	label1->setPosition(90, 560);
+	label1->setTextSize(18);
+
+	gui.add(label1);
+
+	tgui::Label::Ptr label2 = tgui::Label::create();
+	label2->setText("times.    And");
+	label2->setSize(127.2, 38);
+	label2->setPosition(450, 560);
+	label2->setTextSize(18);
+
+	gui.add(label2);
+
+	tgui::ComboBox::Ptr repeatNum = tgui::ComboBox::create();
+	repeatNum->addItem("10");
+	repeatNum->addItem("50");
+	repeatNum->addItem("100");
+	repeatNum->addItem("500");
+	repeatNum->addItem("10000");
+	repeatNum->setPosition(270, 560);
+	repeatNum->setSize(160, 22);
+	repeatNum->setTextSize(18);
+
+	tgui::ComboBox::Ptr choice = tgui::ComboBox::create();
+	choice->addItem("Keep the choice.");
+	choice->addItem("Change the choice.");
+	choice->setPosition(600, 560);
+	choice->setSize(200, 22);
+	choice->setTextSize(18);
+
+	gui.add(repeatNum);
+	gui.add(choice);
 	while (mWindow.isOpen())
 	{
 		sf::Event event;
